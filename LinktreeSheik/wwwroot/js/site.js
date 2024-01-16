da43fd9c2d1064ea20d791fa83121ac5d0ea388d
MarcosVinicius <marcusvinycius73@gmail.com>
2024-01-16 20:02:23 -0300

    Initial commit

---
+++ LinktreeSheik/wwwroot/js/site.js
@@ -0,0 +1,14 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets
+const track = document.getElementById('track');
+const body = document.querySelector('body')
+
+
+track.addEventListener('click', () => {
+    track.classList.toggle('dark')
+    body.classList.toggle('dark')
+    link.classList.toggle('dark')
+})
+
+
+
